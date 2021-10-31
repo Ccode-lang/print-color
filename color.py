dfbc1d7220a04054de69f9b4b3971a43a8523947
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2021-10-31 14:35:20 -0700

    oops

--- color.py
+++ color.py
@@ -3,17 +3,17 @@ def cprint(text, color):
         code = "\033[1;30m"
     elif color == "red":
         code = "\033[1;31m"
-    elif color = "green":
+    elif color == "green":
         code = "\033[1;32m"
-    elif color = "yellow":
+    elif color == "yellow":
         code = "\033[1;33m"
-    elif color = "blue":
+    elif color == "blue":
         code = "\033[1;34m"
-    elif color = "magenta":
+    elif color == "magenta":
         code = "\033[1;35m"
-    elif color = "cyan":
+    elif color == "cyan":
         code = "\033[1;36m"
-    elif color = "white":
+    elif color == "white":
         code = "\033[1;37m"
     else:
         raise Exception("unknown color")
